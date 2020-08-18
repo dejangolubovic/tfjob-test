7c3c558fa791a1c53170b4f75b8960949bb7fa96
Dejan Golubovic <dejan.golubovic.27@gmail.com>
2020-08-18 17:32:26 +0200

    Adding multyworker strategy

--- mnist-kale-katib-gpu.py
+++ mnist-kale-katib-gpu.py
@@ -47,18 +47,19 @@ print("Number of filtered test examples:", len(x_test))
 
 
 # In[18]:
+strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
 
+with strategy.scope():
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(32, [3, 3], activation='relu', input_shape=(28,28,1)))
+    model.add(tf.keras.layers.Conv2D(64, [3, 3], activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(tf.keras.layers.Dropout(0.25))
+    model.add(tf.keras.layers.Flatten())
 
-model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Conv2D(32, [3, 3], activation='relu', input_shape=(28,28,1)))
-model.add(tf.keras.layers.Conv2D(64, [3, 3], activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(tf.keras.layers.Dropout(0.25))
-model.add(tf.keras.layers.Flatten())
-
-model.add(tf.keras.layers.Dense(nodes_number, activation='relu'))
-model.add(tf.keras.layers.Dropout(0.5))
-model.add(tf.keras.layers.Dense(1))
+    model.add(tf.keras.layers.Dense(nodes_number, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.5))
+    model.add(tf.keras.layers.Dense(1))
 
 model.compile(loss='binary_crossentropy', optimizer=tf.keras.optimizers.Adam(learning_rate), metrics=['accuracy'])
 model.summary()
@@ -74,7 +75,8 @@ print(x_train.shape)
 
 
 start = time.time()
-model.fit(x_train, y_train, batch_size=128, epochs=200, verbose=1, validation_data=(x_test, y_test))
+with strategy.scope():
+    model.fit(x_train, y_train, batch_size=128, epochs=200, verbose=1, validation_data=(x_test, y_test))
 end = time.time()
 print('Training took', end - start, 'seconds')
 print('img/sec =', x_train.shape[0] / (end - start))
